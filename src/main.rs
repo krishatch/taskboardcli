650f9a4406b84383ccc5e6066f21db2664577070
krishatch <krishatch1201@gmail.com>
2024-02-07 18:12:04 -0600

    Basic TUI generated. Copyright added.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,105 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, stdout};
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{prelude::*, widgets::*};
+
+/*** Taskboard specific includes ***/
+use chrono::prelude::*;
+use rand::{distributions::Alphanumeric, prelude::*};
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+use thiserror::Error;
+
+const DB_PATH: &str = "../data/lists.json";
+/*** Error handling for db reading ***/
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
 }
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Task {
+    title: String,
+    due: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TaskList {
+    size: usize,
+    name: String,
+    tasks: Vec<Task>,
+}
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    stdout().execute(EnterAlternateScreen)?;
+    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    let mut should_quit = false;
+    while !should_quit {
+        terminal.draw(ui)?;
+        should_quit = handle_events()?;
+    }
+
+    disable_raw_mode()?;
+    stdout().execute(LeaveAlternateScreen)?;
+    Ok(())
+}
+
+fn handle_events() -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(50))? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(true);
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn ui(frame: &mut Frame) {
+    /*** Set up default layout ***/
+    let size = frame.size();
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(2)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(2),
+            Constraint::Length(3),
+        ].as_ref(),
+        )
+        .split(size);
+
+    let copyright = Paragraph::new("taskboardcli 2024 - all rights reserved")
+        .style(Style::default().fg(Color::Red))
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::Green))
+                .title("Copyright")
+                .border_type(BorderType::Plain),
+        );
+
+    frame.render_widget(copyright, chunks[0]);
+
+    // frame.render_widget(
+    //     Paragraph::new("Hello World!")
+    //         .block(Block::default().title("Greeting").borders(Borders::ALL)),
+    //     frame.size(),
+    // );
+}
+
+// fn read_db() -> Result<Vec<TaskList>, Error> {
+//     let db_content = fs::read_to_string(DB_PATH)?;
+// }
