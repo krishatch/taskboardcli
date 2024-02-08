e568e73e169fd7e7e0084f62dfd333984f424db1
krishatch <krishatch1201@gmail.com>
2024-02-07 20:14:37 -0600

    Semi-dynamic task board and help menu created

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::thread;
 use std::time::{Duration, Instant};
 use thiserror::Error;
 
-const DB_PATH: &str = "../data/lists.json";
+const DB_PATH: &str = "./data/lists.json";
 /*** Error handling for db reading ***/
 #[derive(Error, Debug)]
 pub enum Error {
@@ -80,26 +80,151 @@ fn ui(frame: &mut Frame) {
         )
         .split(size);
 
-    let copyright = Paragraph::new("taskboardcli 2024 - all rights reserved")
-        .style(Style::default().fg(Color::Red))
+    /*** Main Taskboard ***/
+    //let tasklists = read_db().expect("can fetch tasklist list");
+    let listlen = 2;
+
+
+    let help_info = Line::from(vec![
+        Span::styled(
+            "<num>",
+            Style::default()
+                .fg(Color::Rgb(0xcc, 0x55, 0x00))
+                .add_modifier(Modifier::UNDERLINED),
+        ),
+        Span::styled(
+            " Select List - ",
+            Style::default()
+                .fg(Color::Rgb(0xff, 0xff, 0xff))
+        ),
+        Span::styled(
+            "N",
+            Style::default()
+                .fg(Color::Rgb(0xcc, 0x55, 0x00))
+                .add_modifier(Modifier::UNDERLINED),
+        ),
+        Span::styled(
+            "ew List - ",
+            Style::default()
+                .fg(Color::Rgb(0xff, 0xff, 0xff))
+        ),
+        Span::styled(
+            "D",
+            Style::default()
+                .fg(Color::Rgb(0xcc, 0x55, 0x00))
+                .add_modifier(Modifier::UNDERLINED),
+        ),
+        Span::styled(
+            "elete List - ",
+            Style::default()
+                .fg(Color::Rgb(0xff, 0xff, 0xff))
+        ),
+        Span::styled(
+            "A",
+            Style::default()
+                .fg(Color::Rgb(0xcc, 0x55, 0x00))
+                .add_modifier(Modifier::UNDERLINED),
+        ),
+        Span::styled(
+            "dd item - ",
+            Style::default()
+                .fg(Color::Rgb(0xff, 0xff, 0xff))
+        ),
+        Span::styled(
+            "C",
+            Style::default()
+                .fg(Color::Rgb(0xcc, 0x55, 0x00))
+                .add_modifier(Modifier::UNDERLINED),
+        ),
+        Span::styled(
+            "ross item",
+            Style::default()
+                .fg(Color::Rgb(0xff, 0xff, 0xff))
+        ),
+        ]
+    );
+
+    let help = Paragraph::new(help_info.clone())
+        .style(Style::default().fg(Color::Gray))
         .alignment(Alignment::Center)
         .block(
             Block::default()
                 .borders(Borders::ALL)
-                .style(Style::default().fg(Color::Green))
+                .style(Style::default().fg(Color::Rgb(0xcc, 0x55, 0x00)))
+                .title("Commands")
+                .border_type(BorderType::Plain),
+            );
+    frame.render_widget(help, chunks[0]);
+    match listlen{
+        0 => {
+            let taskboard = Paragraph::new("No Lists")
+                .style(Style::default().fg(Color::Rgb(0xFF, 0xFF, 0xFF)))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::Rgb(0xcc, 0x55, 0x00)))
+                        .title("Taskboard")
+                        .border_type(BorderType::Plain),
+                );
+            frame.render_widget(taskboard, chunks[1]);
+        }
+        _=>{
+            let mut lists = vec![];
+            for _i in 0..2{
+                lists.push(Constraint::Min(0));
+            }
+            let taskboard = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints(lists)
+                .split(chunks[1]);
+
+            let left = Paragraph::new("List 1")
+                .style(Style::default().fg(Color::Rgb(0xFF, 0xFF, 0xFF)))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::Rgb(0xcc, 0x55, 0x00)))
+                        .title("Taskboard")
+                        .border_type(BorderType::Plain),
+                );
+
+            let right = Paragraph::new("List 2")
+                .style(Style::default().fg(Color::Rgb(0xFF, 0xFF, 0xFF)))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::Rgb(0xcc, 0x55, 0x00)))
+                        .title("Taskboard")
+                        .border_type(BorderType::Plain),
+                );
+            frame.render_widget(left, taskboard[0]);
+            frame.render_widget(right, taskboard[1]);
+        }
+    }
+    let copyright = Paragraph::new("taskboardcli 2024 - all rights reserved")
+        .style(Style::default().fg(Color::Rgb(0xFF, 0xFF, 0xFF)))
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::Rgb(0xcc, 0x55, 0x00)))
                 .title("Copyright")
                 .border_type(BorderType::Plain),
         );
 
-    frame.render_widget(copyright, chunks[0]);
-
-    // frame.render_widget(
-    //     Paragraph::new("Hello World!")
-    //         .block(Block::default().title("Greeting").borders(Borders::ALL)),
-    //     frame.size(),
-    // );
+    /*** Render widgets ***/
+    frame.render_widget(copyright, chunks[2]);
 }
 
-// fn read_db() -> Result<Vec<TaskList>, Error> {
-//     let db_content = fs::read_to_string(DB_PATH)?;
+// fn render_taskboard<'a>(tasklist_list_state: &ListState) -> Paragraph<'a>{
+//
 // }
+
+fn read_db() -> Result<Vec<TaskList>, Error> {
+    let db_content = fs::read_to_string(DB_PATH)?;
+    let parsed: Vec<TaskList> = serde_json::from_str(&db_content)?;
+    Ok(parsed)
+}
